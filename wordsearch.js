4168f59a12fa1829cd9fddf0beb5b6967be6d32f
Roberto Cervantes Betancourt <roberto.cervantes.bet@gmail.com>
2022-06-17 20:25:58 +0000

    fix wordsearch and add new test

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,75 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    const horizontalJoin = letters.map(ls => ls.join(''));
+
+    let newMatrix = [];
+    let tempArray = [];
+    const yLength = letters.length;
+    const xLength = letters[0].length;
+    
+    for (let x = 0; x < xLength; x++){
+        for (let y = 0; y < yLength; y++){
+        tempArray.push(letters[y][x]);
+        }
+        newMatrix.push(tempArray);
+        tempArray = [];
+    } 
+
+    let verticalJoin = newMatrix.map(vl => vl.join(''));
+
+
+
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+        //console.log(l)
+        if (l.includes(word)) return true;
+    }  //return false;
+
+    for (l of verticalJoin) {
+        //console.log(l)
+        if (l.includes(word)) return true;
+    }  //return false;
+
+    return false;
+
+
+      
+    
+    
 }
 
 module.exports = wordSearch
+
+console.log(wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'SEINFELD'))
+
+  console.log(wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'FRANK'))
+
+  console.log(wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'LARRY'))
